2977d650bc2dc909577576bb61e67036b7b31a14
= <=>
2019-10-10 17:01:29 +0700

    k

---
+++ week1/practic/DelarVari/src/DelarVari.java
@@ -0,0 +1,19 @@
+public class DelarVari {
+    public static void main(String[] args) {
+        int i=10;
+        char c ='a';
+        boolean b = true;
+        String str = "le xuân hải";
+        double  d = 2.4;
+        long  l = 23000;
+        float f =2.4f;
+        System.out.println("i = "+i);
+        System.out.println("c = "+c);
+        System.out.println("b = "+b);
+        System.out.println("str = "+str);
+        System.out.println("d = "+d);
+        System.out.println("l = "+l);
+        System.out.println("f = "+f);
+
+    }
+}
