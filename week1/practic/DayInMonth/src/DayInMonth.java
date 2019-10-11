128fe7b6979fd2c997f7fb719a7a22e2e74c099d
= <=>
2019-10-11 08:30:00 +0700

    day

--- week1/practic/DayInMonth/src/DayInMonth.java
+++ week1/practic/DayInMonth/src/DayInMonth.java
@@ -2,13 +2,14 @@ import java.util.Scanner;
 
 public class DayInMonth {
     public static void main(String[] args) {
-        int month;
+
         Scanner sc =new Scanner(System.in);
         System.out.println("Nhập tháng để kiểm tra");
-        month =sc.nextInt();
-        switch (month){
+        int month =sc.nextInt();
+        String dayIsMonth;
+        switch (month) {
             case 2:
-                System.out.println("Tháng 2 có 28 ngày");
+                dayIsMonth = "28";
                 break;
             case 1:
             case 3:
@@ -17,19 +18,21 @@ public class DayInMonth {
             case 12:
             case 8:
             case 10:
-                System.out.println("Tháng "+ month + "có 31 ngày");
+                dayIsMonth = "31";
                 break;
             case 4:
             case 6:
             case 11:
             case 9:
-                System.out.println("Tháng " + month + "có 30 ngày");
+                dayIsMonth = "30";
                 break;
             default:
-                System.out.println("Please chose another month");
+                dayIsMonth = "";
                 break;
-
+        }
+        if (dayIsMonth != "") System.out.printf("the month '%d' có '%s' day ! "  ,month,dayIsMonth);
+        else System.out.println("invalid");
 
         }
     }
-}
+
