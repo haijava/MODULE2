f27eca2b95c294282472a22f182b460c24efb73e
= <=>
2019-10-11 14:43:54 +0700

    great

---
+++ week1/practic/GreatestDivi/src/GreatestDivi.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class GreatestDivi {
+    public static  void FindGreatDivi(){
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Nhập số a");
+       int  a =sc.nextInt();
+        System.out.println("Nhập số b");
+        int b =sc.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a==0||b==0) System.out.println("không có ước chung lớn nhất ");
+        while (a!=b){
+            if (a>b) a=a-b;
+            else
+                b=b-a;
+            System.out.println("ucln "+a);
+        }
+    }
+    public static void main(String[] args) {
+    FindGreatDivi();
+    }
+}
